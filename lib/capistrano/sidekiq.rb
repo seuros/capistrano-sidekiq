484a7656b5233bbcdfd77f0e9a8c5b0d33d6d922
Jérémy Lecour <jeremy.lecour@gmail.com>
2014-03-17 12:24:36 +0100

    More robust version checking
    With Capistrano 2.15, there is no `Capistrano::VERSION` constant, only a `Capistrano::Version` method.

--- lib/capistrano/sidekiq.rb
+++ lib/capistrano/sidekiq.rb
@@ -1,4 +1,10 @@
-if Gem::Version.new(Capistrano::VERSION).release >= Gem::Version.new('3.0.0')
+version = begin
+  Capistrano::VERSION
+rescue NameError
+  Capistrano::Version
+end
+
+if Gem::Version.new(version).release >= Gem::Version.new('3.0.0')
   load File.expand_path('../tasks/sidekiq.cap', __FILE__)
 else
   require_relative 'tasks/capistrano2'
