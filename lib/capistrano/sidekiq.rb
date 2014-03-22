82e0c5785fba1b37d69e60b16606aad0eef865cd
Judeuros <lamhaouihajar@gmail.com>
2014-03-22 08:36:32 -0500

    Clean version checking

--- lib/capistrano/sidekiq.rb
+++ lib/capistrano/sidekiq.rb
@@ -1,10 +1,4 @@
-version = begin
-  Capistrano::VERSION
-rescue NameError
-  Capistrano::Version
-end
-
-if Gem::Version.new(version).release >= Gem::Version.new('3.0.0')
+if Gem::Specification::find_by_name('capistrano').version >= Gem::Version.new('3.0.0')
   load File.expand_path('../tasks/sidekiq.cap', __FILE__)
 else
   require_relative 'tasks/capistrano2'
