b73bd8d0e49057265dc1079f5090f87ab99ea73e
Abdelkader Boudih <terminale@gmail.com>
2022-09-10 23:45:04 +0100

    feat: sync with capistrano3-puma

--- lib/capistrano/sidekiq/systemd.rb
+++ lib/capistrano/sidekiq/systemd.rb
@@ -5,7 +5,8 @@ module Capistrano
     include Sidekiq::Helpers
 
     def set_defaults
-      set_if_empty :sidekiq_service_unit_name, 'sidekiq' # This will change in version 3.0.0 to {application}_sidekiq_{stage}
+      set_if_empty :sidekiq_systemctl_bin, '/bin/systemctl'
+      set_if_empty :sidekiq_service_unit_name, -> { "sidekiq_#{fetch(:application)}_#{fetch(:stage)}" }
       set_if_empty :sidekiq_service_unit_user, :user # :system
       set_if_empty :sidekiq_enable_lingering, true
       set_if_empty :sidekiq_lingering_user, nil
