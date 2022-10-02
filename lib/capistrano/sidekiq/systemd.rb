510bb88be9a860d378383a8cc86cb38a0b22a19d
Abdelkader Boudih <terminale@gmail.com>
2022-10-02 11:24:54 +0100

    Add support to multiple EnvironmentFile

--- lib/capistrano/sidekiq/systemd.rb
+++ lib/capistrano/sidekiq/systemd.rb
@@ -6,10 +6,13 @@ module Capistrano
 
     def set_defaults
       set_if_empty :sidekiq_systemctl_bin, -> { fetch(:systemctl_bin, '/bin/systemctl') }
-      set_if_empty :sidekiq_service_unit_name, -> { "sidekiq_#{fetch(:application)}_#{fetch(:stage)}" }
+      set_if_empty :sidekiq_service_unit_name, -> { "#{fetch(:application)}_sidekiq_#{fetch(:stage)}" }
       set_if_empty :sidekiq_service_unit_user, -> { fetch(:service_unit_user, :user) }
       set_if_empty :sidekiq_enable_lingering, -> { fetch(:puma_systemctl_user) != :system }
       set_if_empty :sidekiq_lingering_user, -> { fetch(:lingering_user, fetch(:user)) }
+
+      set_if_empty :sidekiq_service_unit_env_files, -> { fetch(:service_unit_env_files, []) }
+      set_if_empty :sidekiq_service_unit_env_vars, -> { fetch(:service_unit_env_vars, []) }
 
       set_if_empty :sidekiq_service_templates_path, fetch(:service_templates_path, 'config/deploy/templates')
     end
